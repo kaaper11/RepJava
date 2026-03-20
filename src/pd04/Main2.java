b6667ae36c7819f71aad5a5dc1bc113e61562ed3
Kacper Ajdyna <ajdynakacper@gmail.com>
2026-03-20 19:54:48 +0100

    PD-04: End exercise about players score, prime numbers and loop pyramid.

---
+++ src/pd04/Main2.java
@@ -0,0 +1,22 @@
+package pd04;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        int j;
+        int licznik;
+        for (int i = 2; i <= 50; i++) {
+            licznik = 0;
+            j = 1;
+            while (i >= j) {
+                if (i % j == 0) {
+                    licznik++;
+                }
+                j++;
+            }
+            if (licznik == 2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
---
+++ src/pd04/Main3.java
@@ -0,0 +1,19 @@
+package pd04;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int iloscSpacji;
+
+        for (int k = 0; k < 5; k++) {
+            iloscSpacji = 0;
+            for (int l = 0; l < 5 - k - 1; l++) {
+                System.out.print(" ");
+                iloscSpacji++;
+            }
+            for (int l = 0; l < 5 - iloscSpacji; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
