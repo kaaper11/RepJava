b40872fdee3ef1f4a20efccdc80850b23f529bd2
Kacper Ajdyna <ajdynakacper@gmail.com>
2026-04-21 14:11:19 +0200

    PD-15: Added ProductAnalytics class which share raports products methods.

--- src/pd15/Main.java
+++ src/pd15/Main.java
@@ -8,7 +8,7 @@ public class Main {
         ProductAnalytics.addProduct(Product.of(2, "prod2", Category.CLOTHES, new BigDecimal("20"), 10, 1, 4));
         ProductAnalytics.addProduct(Product.of(3, "prod3", Category.ELECTRONICS, new BigDecimal("10100"), 20, 0, 4.2));
         ProductAnalytics.addProduct(Product.of(4, "prod4", Category.FURNITURE, new BigDecimal("500"), 5, 10, 3.4));
-        ProductAnalytics.addProduct(Product.of(5, "prod5", Category.OTHER, new BigDecimal("180"), 100, 20, 5));
+        ProductAnalytics.addProduct(Product.of(5, "prod6", Category.OTHER, new BigDecimal("180"), 100, 20, 5));
 
         System.out.println(ProductAnalytics.generateTopCategoryProductReport());
         System.out.println(ProductAnalytics.findCategoryWithHighestAverageRating());
