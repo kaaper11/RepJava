0eaac45f3b9143040ead9a4c71fe52a67b8d9395
Kacper Ajdyna <ajdynakacper@gmail.com>
2026-04-29 19:43:18 +0200

    PD-17: Added save category statistics to json file.

--- src/pd17/Main.java
+++ src/pd17/Main.java
@@ -8,14 +8,15 @@ public class Main {
     public static void main(String[] args) {
         try {
             SalesCsvReader csvReader = new SalesCsvReader();
-            SalesAnalytics salesAnalytics = new SalesAnalytics(csvReader.read(Path.of("Z:\\Projects\\RepJava2\\src\\pd17\\sales.csv")));
+            SalesAnalytics salesAnalytics = new SalesAnalytics(csvReader.read(Path.of("src\\pd17\\sales.csv")));
             System.out.println(salesAnalytics.getTotalRevenue());
             System.out.println(salesAnalytics.top3Products());
             List<CategorySummary> categorySummaries = salesAnalytics.getCategorySummary();
 
             SalesReportWriter salesReportWriter = new SalesReportWriter();
-            salesReportWriter.safe("Z:\\Projects\\RepJava2\\src\\pd17\\sales1.csv", categorySummaries);
-        }catch (IOException e) {
+            salesReportWriter.saveToJson(categorySummaries, "src\\pd17\\sales3.json");
+            salesReportWriter.saveToCsv("src\\pd17\\sales1.csv", categorySummaries);
+        } catch (IOException e) {
             System.err.println("Brak pliku w podanej ścieżce!");
         }
 
--- src/pd17/SalesReportWriter.java
+++ src/pd17/SalesReportWriter.java
@@ -1,19 +1,27 @@
 package pd17;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 public class SalesReportWriter {
-    public void safe(String path, List<CategorySummary> categorySummaries) throws IOException {
+    public void saveToCsv(String path, List<CategorySummary> categorySummaries) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(path));
         writer.write("category,mean_price,revenue\n");
 
         for (CategorySummary categorySummary : categorySummaries) {
             writer.write(oneLine(categorySummary));
         }
+    }
+
+    public void saveToJson(List<CategorySummary> categorySummaries, String filePath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File(filePath), categorySummaries);
     }
 
     private String oneLine(CategorySummary summary) {
