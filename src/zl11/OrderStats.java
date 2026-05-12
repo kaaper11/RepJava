3ffd38f002d0d2e5c08fbc9330cb2165effe8b53
Kacper Ajdyna <ajdynakacper@gmail.com>
2026-05-12 22:32:08 +0200

    PD-20: Added async LaptopStore.

---
+++ src/zl11/OrderStats.java
@@ -0,0 +1,11 @@
+package zl11;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class OrderStats {
+    private boolean orderResult;
+    private int quantity;
+}
